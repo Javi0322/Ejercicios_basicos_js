e695d8f8b0e79fa31a3ef1a15aaea0ee07b2896f
Javier Garcia Manotas <javigarciaman@gmail.com>
2025-03-03 22:40:54 +0100

    ejercicio4

---
+++ ejercicio4.js
@@ -0,0 +1,25 @@
+//Ejercicio 4
+const aldeanos = ['Fibrilio', 'Narciso', 'Vacarena', 'Tendo', 'Nendo']
+
+//4.1
+console.log(aldeanos[3])
+
+//4.2
+aldeanos.push('Cervasio')
+console.log(aldeanos)
+
+//4.3
+aldeanos[0] = 'Bambina'
+console.log(aldeanos[0])
+
+//4.4
+aldeanos.reverse()
+console.log(aldeanos)
+aldeanos.reverse()
+
+//4.5
+aldeanos.splice(1, 1, 'Canela')
+console.log(aldeanos[1])
+
+//4.6
+console.log(aldeanos[aldeanos.length - 1])
